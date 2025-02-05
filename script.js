2db81a63257e7b41f64b28e7c0b7320185ef3d08
rea997 <rea.susuri997@gmail.com>
2025-02-05 18:59:36 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+var dita = prompt("Kallxoma numrin e dites une te tregoj cilla dite eshte?")
+
+switch( parseInt (dita) ) {
+    case 1:
+        alert("E hene");
+        break;
+    case 2:
+        alert("E marte");
+        break;
+    case 3:
+        alert("E merkure");
+        break;
+    case 4:
+        alert("E enjte");
+        break;
+    case 5:
+        alert("E premte");
+        break;
+    case 6:
+        alert("E shtune");
+        break;
+    case 7:
+        alert("E dielle");
+        break;
+}
